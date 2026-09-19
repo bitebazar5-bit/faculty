369ba20342c8bb14b3f0079bc3ad92a9c342f0ec
bitebazar5-bit <bitebazar5@gmail.com>
2026-09-19 06:28:08 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAd1ZgZLEeF9DJt7t6pdHY3hPlOrOeuXVs",
+  authDomain: "faculty-5e149.firebaseapp.com",
+  projectId: "faculty-5e149",
+  storageBucket: "faculty-5e149.firebasestorage.app",
+  messagingSenderId: "1076061236942",
+  appId: "1:1076061236942:web:4a13db30f60239b9a46a9f",
+  measurementId: "G-BT0N5H2M8G"
+};
